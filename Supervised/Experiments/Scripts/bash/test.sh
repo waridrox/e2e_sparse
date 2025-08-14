b5373d99aafccc942cd24f762cbd3a475511113c
Mohammad Warid <mohdwarid4@gmail.com>
2025-08-14 21:56:29 +0530

    Implement data loading and training loop in test script

--- Supervised/Experiments/Scripts/bash/test.sh
+++ Supervised/Experiments/Scripts/bash/test.sh
@@ -6,6 +6,7 @@ BASE_DIR=${PSCRATCH}/e2e_sparse
 
 
 ########################################################################
+
 CUDA_VISIBLE_DEVICES=0 python3 ${BASE_DIR}/Supervised/trainer.py \
   --datapath=${BASE_DIR}/Dataset/QG1024.h5 \
   --Nepochs=100 \
@@ -15,7 +16,7 @@ CUDA_VISIBLE_DEVICES=0 python3 ${BASE_DIR}/Supervised/trainer.py \
   --wandb_project=SparseCNN \
   --wandb_entity=e2e_sparse \
   --wandb_run_name=ResNet_PC_1024_S \
-  --wandb_key=$wandb_key
+  --wandb_key=$wandb_key &
 
 
   CUDA_VISIBLE_DEVICES=0 python3 ${BASE_DIR}/Supervised/trainer.py \
@@ -27,7 +28,7 @@ CUDA_VISIBLE_DEVICES=0 python3 ${BASE_DIR}/Supervised/trainer.py \
   --wandb_project=SparseCNN \
   --wandb_entity=e2e_sparse \
   --wandb_run_name=ResNet_PC_1024_M \
-  --wandb_key=$wandb_key
+  --wandb_key=$wandb_key &
 ########################################################################
 
 CUDA_VISIBLE_DEVICES=1 python3 ${BASE_DIR}/Supervised/trainer.py \
@@ -39,8 +40,7 @@ CUDA_VISIBLE_DEVICES=1 python3 ${BASE_DIR}/Supervised/trainer.py \
   --wandb_project=SparseCNN \
   --wandb_entity=e2e_sparse \
   --wandb_run_name=ResNet_PC_768_S \
-  --wandb_key=$wandb_key
-
+  --wandb_key=$wandb_key &
 
   CUDA_VISIBLE_DEVICES=1 python3 ${BASE_DIR}/Supervised/trainer.py \
   --datapath=${BASE_DIR}/Dataset/QG768.h5 \
@@ -51,5 +51,55 @@ CUDA_VISIBLE_DEVICES=1 python3 ${BASE_DIR}/Supervised/trainer.py \
   --wandb_project=SparseCNN \
   --wandb_entity=e2e_sparse \
   --wandb_run_name=ResNet_PC_768_M \
-  --wandb_key=$wandb_key
+  --wandb_key=$wandb_key &
+
 ########################################################################
+
+CUDA_VISIBLE_DEVICES=2 python3 ${BASE_DIR}/Supervised/trainer.py \
+  --datapath=${BASE_DIR}/Dataset/QG512.h5 \
+  --Nepochs=100 \
+  --lr=1e-3 \
+  --model_variant=ResNet_PC_512_S \
+  --UseWandb=True \
+  --wandb_project=SparseCNN \
+  --wandb_entity=e2e_sparse \
+  --wandb_run_name=ResNet_PC_512_S \
+  --wandb_key=$wandb_key &
+
+
+  CUDA_VISIBLE_DEVICES=1 python3 ${BASE_DIR}/Supervised/trainer.py \
+  --datapath=${BASE_DIR}/Dataset/QG512.h5 \
+  --Nepochs=100 \
+  --lr=1e-3 \
+  --model_variant=ResNet_PC_512_M \
+  --UseWandb=True \
+  --wandb_project=SparseCNN \
+  --wandb_entity=e2e_sparse \
+  --wandb_run_name=ResNet_PC_512_M \
+  --wandb_key=$wandb_key &
+
+########################################################################
+
+CUDA_VISIBLE_DEVICES=2 python3 ${BASE_DIR}/Supervised/trainer.py \
+  --datapath=${BASE_DIR}/Dataset/QG256.h5 \
+  --Nepochs=100 \
+  --lr=1e-3 \
+  --model_variant=ResNet_PC_256_S \
+  --UseWandb=True \
+  --wandb_project=SparseCNN \
+  --wandb_entity=e2e_sparse \
+  --wandb_run_name=ResNet_PC_256_S \
+  --wandb_key=$wandb_key &
+
+  CUDA_VISIBLE_DEVICES=1 python3 ${BASE_DIR}/Supervised/trainer.py \
+  --datapath=${BASE_DIR}/Dataset/QG256.h5 \
+  --Nepochs=100 \
+  --lr=1e-3 \
+  --model_variant=ResNet_PC_256_M \
+  --UseWandb=True \
+  --wandb_project=SparseCNN \
+  --wandb_entity=e2e_sparse \
+  --wandb_run_name=ResNet_PC_256_M \
+  --wandb_key=$wandb_key &
+
+  wait
