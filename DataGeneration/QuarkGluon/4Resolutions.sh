9f6f862d616f124ed8cc1acff800bc5b78685fda
Mohammad Warid <mohdwarid4@gmail.com>
2025-08-14 21:40:24 +0530

    Fix output file paths in 4Resolutions script

--- DataGeneration/QuarkGluon/4Resolutions.sh
+++ DataGeneration/QuarkGluon/4Resolutions.sh
@@ -28,9 +28,9 @@ if [ -f ${BASE_DIR}/Dataset/QG256.h5 ]; then
   rm -rf ${BASE_DIR}/Dataset/QG256.h5
 fi
 
-python3 ToPointCloud.py --input_file=input.h5 --output_file=${BASE_DIR}/Dataset/QG1024.h5 --num_points=1024 &
-python3 ToPointCloud.py --input_file=input.h5 --output_file=${BASE_DIR}/Dataset/QG768.h5 --num_points=768 &
-python3 ToPointCloud.py --input_file=input.h5 --output_file=${BASE_DIR}/Dataset/QG512.h5 --num_points=512 &
-python3 ToPointCloud.py --input_file=input.h5 --output_file=${BASE_DIR}/Dataset/QG256.h5 --num_points=256 &
+python3 ToPointCloudForm.py --input_file=input.h5 --output_file=${BASE_DIR}/Dataset/QG1024.h5 --num_points=1024 &
+python3 ToPointCloudForm.py --input_file=input.h5 --output_file=${BASE_DIR}/Dataset/QG768.h5 --num_points=768 &
+python3 ToPointCloudForm.py --input_file=input.h5 --output_file=${BASE_DIR}/Dataset/QG512.h5 --num_points=512 &
+python3 ToPointCloudForm.py --input_file=input.h5 --output_file=${BASE_DIR}/Dataset/QG256.h5 --num_points=256 &
 
 wait
