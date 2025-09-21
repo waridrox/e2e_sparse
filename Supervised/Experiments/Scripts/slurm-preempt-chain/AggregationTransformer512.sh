0692f4443babcc938361ce27d0b7f4b7a17000f3
dc250601 <cdiptarko@gmail.com>
2025-09-22 03:38:56 +0530

    big

---
+++ Supervised/Experiments/Scripts/slurm-preempt-chain/AggregationTransformer512.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH -A m4392
+#SBATCH -C gpu
+#SBATCH -q preempt
+#SBATCH -t 48:00:00
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-task=4
+#SBATCH --job-name=AggregationTransformer512
+
+timeout_handler()
+{
+    scontrol requeue ${SLURM_JOB_ID}
+}
+
+
+module load tensorflow/2.12.0
+
+BASE_DIR=${PSCRATCH}/e2e_sparse
+
+run_id=$1
+
+########################################################################
+cp ${BASE_DIR}/Dataset/QG512.h5 /dev/shm/ &
+
+wait
+
+########################################################################
+bash ${BASE_DIR}/Supervised/Experiments/Scripts/bash/AggregationTransformer512.sh ${run_id} &
+
+trap "timeout_handler" USR1
+
+wait
